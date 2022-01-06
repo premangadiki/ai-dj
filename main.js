d4340a6d87d7a2ea1489fb7a6aa87be1ae074647
premangadiki <87991780+premangadiki@users.noreply.github.com>
2022-01-06 20:24:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
 function setup(){
     canvas = createCanvas(600 , 500);
     canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
 }
-
-song ="";
-
-function draw(){
-    image(video , 0, 0, 600, 500);
-}
- 
-function preload(){
-    song = loadSound("music.mp3");
-}
- function Play(){
-   song.play();
- }
